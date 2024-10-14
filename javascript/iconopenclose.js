f04be04714f85a5d06cdbd7f4c944d2e4a676430
shreypadaliya <shreypadaliya@gmail.com>
2024-10-14 16:21:06 +0530

    update apply for investment button

---
+++ javascript/iconopenclose.js
@@ -0,0 +1,7 @@
+// Get the applyinvesticon button
+const applyButtonIcon = document.getElementById("applyButtonIcon");
+
+// Show div when applyButtonIcon is clicked
+applyButtonIcon.addEventListener("click", function () {
+  investmentDiv.style.display = "block"; // Show the investment div
+});
